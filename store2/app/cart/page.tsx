459e7b4433675de6621259e9724a8b541556da02
samson-kaleti <samson.kaleti@calibrage.in>
2024-10-25 12:28:05 +0530

    added back button in order-conform-page

--- store2/app/cart/page.tsx
+++ store2/app/cart/page.tsx
@@ -202,25 +202,32 @@ const CartPage = () => {
                     <tr key={item.id} className="border-b">
                       <td className="py-4">
                         <div className="flex items-center space-x-4">
-                          <div className="relative w-10 h-20">
-                            <Image
-                              src={item.backgroundTShirt.url}
-                              alt={item.title}
-                              layout="fill"
-                              objectFit="cover"
-                              className="rounded-md"
-                              style={{ backgroundColor: item.backgroundTShirt.color }}
-                            />
-                            <Image
-                              src={item.thumbnail}
-                              alt={item.title}
-                              layout="fill"
-                              objectFit="contain"
-                              className="rounded-md" 
-                              
-                              
-                              
-                            />
+                        <div className="relative w-20 h-24 flex-shrink-0">
+                            {/* Background T-shirt image */}
+                            <div className="absolute inset-0">
+                              <Image
+                                src={item.backgroundTShirt.url}
+                                alt="T-shirt background"
+                                layout="fill"
+                                objectFit="contain"
+                                className="rounded-md"
+                                style={{
+                                  backgroundColor: item.backgroundTShirt.color,
+                                }}
+                              />
+                            </div>
+                            {/* Overlay thumbnail image */}
+                            <div className="absolute inset-0 flex items-center justify-center">
+                              <div className="relative w-1/2 h-1/2 translate-y-[-10%]">
+                                <Image
+                                  src={item.thumbnail}
+                                  alt={item.title}
+                                  layout="fill"
+                                  objectFit="contain"
+                                  className="rounded-md"
+                                />
+                              </div>
+                            </div>
                           </div>
                           <div>
                             <h3 className="font-medium">{item.title}</h3>
