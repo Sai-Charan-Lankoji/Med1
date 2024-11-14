159891bc477b005747347201f8bd0c41c886a487
praveenkumartalari <praveen.talari@calibrage.in>
2024-11-14 16:36:53 +0530

    Fixing user login issue

--- vendor/src/app/hooks/auth/useVendorLogin.ts
+++ vendor/src/app/hooks/auth/useVendorLogin.ts
@@ -11,6 +11,11 @@ interface VendorLoginResponse {
     business_type: string;
     company_name: string;
   };
+  vendorUser: {
+    email: string;
+    first_name: string;
+    vendor_id: string;
+  }
 }
 
 export const useVendorLogin = () => {
@@ -44,15 +49,21 @@ export const useVendorLogin = () => {
   
       const data: VendorLoginResponse = await response.json();
       if (data.token) {
-        console.log('Login successful'); 
-        sessionStorage.setItem('email',data.vendor.contact_email)
-        sessionStorage.setItem('contactName', data.vendor.contact_name)
-        sessionStorage.setItem('vendor_id', data.vendor.id); 
-        sessionStorage.setItem('business_type', data.vendor.business_type);
-        sessionStorage.setItem('company_name', data.vendor.company_name);
-        setAuthEmail(data.vendor.contact_email);
-        setContactName(data.vendor.contact_name);
-        
+        console.log('Login successful');
+        if(data.vendor){
+          sessionStorage.setItem('email',data.vendor.contact_email)
+          sessionStorage.setItem('contactName', data.vendor.contact_name)
+          sessionStorage.setItem('vendor_id', data.vendor.id); 
+          sessionStorage.setItem('business_type', data.vendor.business_type);
+          sessionStorage.setItem('company_name', data.vendor.company_name);
+          setAuthEmail(data.vendor.contact_email)
+          setAuthEmail(data.vendor.contact_name)
+        } 
+        else{
+          sessionStorage.setItem('vendor_id', data.vendorUser.vendor_id)
+        setAuthEmail(data.vendorUser.email);
+        setContactName(data.vendorUser.first_name);
+        }
         router.push('/vendor/orders');
       }
     } catch (err: any) {
