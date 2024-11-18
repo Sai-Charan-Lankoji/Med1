5a4cbeefecdd93413c0cca18655b8be28cdadf9d
samson-kaleti <samson.kaleti@calibrage.in>
2024-11-18 18:09:23 +0530

    remove store

--- store/constants/constant.ts
+++
@@ -1,7 +0,0 @@
-export const NEXT_PUBLIC_API_URL= "http://localhost:9000"
-
-export const NEXT_PUBLIC_API_KEY= "pk_01J9JQNA1HVN8XRFV8PGNAJHP0"
-
-export const NEXT_PUBLIC_VENDOR_ID= "vendor_01J98V6VYJ89KZWNRSEF8G669C"
-
-export const NEXT_STORE_NAME = "Customized Baseball Jersey Design"
