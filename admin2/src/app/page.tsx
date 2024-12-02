6b7394d6ce154f1af134d7f2e42b3df6a95138b5
samson-kaleti <samson.kaleti@calibrage.in>
2024-12-02 12:12:37 +0530

    resolved all build issues in admin2

--- admin2/src/app/page.tsx
+++ admin2/src/app/page.tsx
@@ -1,5 +1,4 @@
 "use client"
-import { Navigation } from "@/components/navigation";
 import Login from "./login/page";
 import React from "react"
 //import { PlanSelection } from "@/components/planselection";
